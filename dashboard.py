1f59a47edc07625ede5825c51160dec1a82e2a06
specialproject305-cmyk <specialproject305@gmail.com>
2026-06-13 22:42:11 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -133,7 +133,7 @@ def create_stacked_bar(df, group_col, categories, colors, title=""):
     fig.update_layout(
         barmode='stack',
         margin=dict(l=0, r=0, t=40, b=0),
-        height=2200,
+       180,
         paper_bgcolor='rgba(0,0,0,0)',
         plot_bgcolor='rgba(0,0,0,0)',
         font=dict(color='#475569'),
