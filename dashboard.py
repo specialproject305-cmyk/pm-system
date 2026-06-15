f43aeca58e0c00bbad08bed1fa3352fcd214fb64
specialproject305-cmyk <specialproject305@gmail.com>
2026-06-15 10:41:40 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -406,16 +406,16 @@ def dashboard_page():
                 legend={
                     'orientation': "h",        # Horizontal layout
                     'yanchor': "top",          # Jangkar di atas kotak legenda
-                    'y': -0.3,                # Didorong lebih bawah lagi (-0.22) agar menjauh dari nama vendor
+                    'y': -0.22,                # Didorong lebih bawah lagi (-0.22) agar menjauh dari nama vendor
                     'xanchor': "center",
                     'x': 0.5,
                     'font': {'size': 9},       # 🌟 DIPERKECIL: Ukuran teks legenda menjadi 9px (Sangat minimalis)
-                    'itemwidth': 20,           # 🌟 DIPERKECIL: Lebar maksimum area item ikon warna
+                    'itemwidth': 30,           # 🌟 DIPERKECIL: Lebar maksimum area item ikon warna
                     'itemsizing': 'constant',  # Memastikan ukuran simbol konisten kecil
                     'traceorder': 'normal'
                 },
                 margin={
-                    't': 40,
+                    't': 35,
                     'b': 110,                  # 🌟 RUANG TAMBAHAN: Jarak bawah dinaikkan ke 110px agar aman
                     'l': 50,
                     'r': 50
